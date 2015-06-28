21f094337eaa2105ebadcd1c4a0c3d277f8b97b8
beauvillain <mbeauvillain@red1-innovation.com>
2015-06-28 20:26:32 +0200

    Ajout du bundle d'annotation de securite pour les roles

--- PRAG/app/AppKernel.php
+++ PRAG/app/AppKernel.php
@@ -30,6 +30,7 @@ class AppKernel extends Kernel
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
             $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
+			$bundles[] = new Lsw\SecureControllerBundle\LswSecureControllerBundle();
         }
 
         return $bundles;
