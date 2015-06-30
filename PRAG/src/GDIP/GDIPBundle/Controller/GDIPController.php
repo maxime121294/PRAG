d36ec1145ca24ac0c07975c2eeada4b7672f9309
Maxime REBIBO <maximerebibo@gmail.com>
2015-06-30 19:24:00 +0200

    Action de l'ordonnancement des pré choix en ajax

--- PRAG/src/GDIP/GDIPBundle/Controller/GDIPController.php
+++ PRAG/src/GDIP/GDIPBundle/Controller/GDIPController.php
@@ -5,6 +5,9 @@ namespace GDIP\GDIPBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class GDIPController extends Controller
 {
@@ -56,5 +59,52 @@ class GDIPController extends Controller
             'user' => $user
 			)
         );
+    }
+
+    /**
+     * @Route("/pre-choix/ordre/valider", name="validationPreChoix")
+     * @Method({"GET", "POST"})
+     */
+    public function validerOrdrePreChoixAction(Request $request)
+    {
+        $ordrePreChoixString = $request->get("ordrePreChoixString");
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->container->get('security.token_storage')->getToken()->getUser();
+        $ordrePreChoixString=str_replace(' ','',$ordrePreChoixString); //enleve les espaces
+        $ordrePreChoixArray = explode(",", $ordrePreChoixString); //convertit en tableau avec les virgules comme separateur
+
+        $repository = $em->getRepository('GDIPGDIPBundle:PreChoix');
+
+        $position = 1;
+
+        foreach ($ordrePreChoixArray as $positionPreChoix) {
+            $query = $repository->createQueryBuilder('p')
+                ->where("p.utilisateur = :user")
+                ->andWhere("p.position = :position")
+                ->andWhere("p.traite = false")
+                ->setParameter("user", $user->getId())
+                ->setParameter("position", $positionPreChoix);
+
+            $preChoix = $query->getQuery()->getSingleResult();
+
+            $preChoix->setPosition($position);
+            $preChoix->setTraite(true);
+            $em->persist($preChoix);
+            $em->flush();
+
+            $position++;
+        }
+
+        $entities = $em->getRepository('GDIPGDIPBundle:PreChoix')->findAll();
+
+        foreach ($entities as $preChoix) {
+            $preChoix->setTraite(false);
+            $em->persist($preChoix);
+            $em->flush();
+        }
+
+        $success['success'] = "success"; 
+
+        return new JsonResponse($success);
     }
 }
