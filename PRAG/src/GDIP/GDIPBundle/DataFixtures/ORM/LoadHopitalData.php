5aa46d14ddb2c0c50b7d67acbf61262bd11c49cd
Nicolas Notari <saucissongras@yahoo.fr>
2015-06-30 12:39:36 +0200

    Fixtures Filière, Hôpital, Service et Poste reliée. PB plusieurs de chaque identifiant dans la base

--- PRAG/src/GDIP/GDIPBundle/DataFixtures/ORM/LoadHopitalData.php
+++ PRAG/src/GDIP/GDIPBundle/DataFixtures/ORM/LoadHopitalData.php
@@ -2,35 +2,43 @@
 
 namespace GDIP\UserBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use GDIP\GDIPBundle\Entity\Hopital;
 
-class LoadHopitalData implements FixtureInterface
+class LoadHopitalData extends AbstractFixture implements OrderedFixtureInterface
 {
     /**
      * {@inheritDoc}
      */
     public function load(ObjectManager $manager)
     {
-        $trousseau = new Hopital();
-        $trousseau->setLibelleHopital('Trousseau');
-        $trousseau->setLieuHopital('Paris');
-        $trousseau->setDeptHopital('75');
-        $manager->persist($trousseau);
+        $hop1 = new Hopital();
+        $hop1->setLibelleHopital('Trousseau');
+        $hop1->setLieuHopital('Paris');
+        $hop1->setDeptHopital('75');
+        $this->addReference('hop1', $hop1);
+        $manager->persist($hop1);
 
-        $cochin = new Hopital();
-        $cochin->setLibelleHopital('Cochin');
-        $cochin->setLieuHopital('Saint-Mandé');
-        $cochin->setDeptHopital('94');
-        $manager->persist($cochin);
-
-        $rotschild = new Hopital();
-        $rotschild->setLibelleHopital('Rotschild');
-        $rotschild->setLieuHopital('Neuilly-sur-Marne');
-        $rotschild->setDeptHopital('93');
-        $manager->persist($rotschild);
+/*
+        $hop2 = new Hopital();
+        $hop2->setLibelleHopital('Cochin');
+        $hop2->setLieuHopital('Saint');
+        $hop2->setDeptHopital('94');
+        $this->addReference('hop2', $hop2);
+        $manager->persist($hop2);
+*/
 
         $manager->flush();
+    }
+
+    /**
+     *
+     * {@inheritDoc}
+     */
+    public function getOrder()
+    {
+        return 1;
     }
 }
